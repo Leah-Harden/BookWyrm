83c0e3266c66c384ef355fa879f579341b2d4574
Leah <leahxharden@gmail.com>
2023-04-10 20:24:57 -0400

    working on homeroutes and added a test seed
    We need to reorganized the routes. apiHomeroutes is the old homeroutes and it need to go in the the api folder but i have not move it so it does not affect other code

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoutes.js');
+//do not change!!! this still works!!!!!
+const apiHomeRoutes = require('./apiHomeRoutes.js');
+
 
 router.use('/', homeRoutes);
+router.use('/api', apiHomeRoutes);
+
 
 module.exports = router;
