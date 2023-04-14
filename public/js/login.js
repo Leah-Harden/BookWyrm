0289f61a16f72316e0a6bf21a482438979a8b783
Norboro <ajbrook19@gmail.com>
2023-04-13 21:28:49 -0400

    Completed login page and functionality of login.js and user routes.

---
+++ public/js/login.js
@@ -0,0 +1,55 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const name = document.querySelector('#name-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (name && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/user/login', {
+        method: 'POST',
+        body: JSON.stringify({ name, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/account');
+      } else {
+        const error = await response.json();
+        alert(error.message);
+      }
+    }
+  };
+  
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#name-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (name && password) {
+      const response = await fetch('/user/signup', {
+        method: 'POST',
+        body: JSON.stringify({ name, password, bookProgress: false }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/account');
+      } else {
+        const error = await response.json();
+        console.log(error);
+        alert(error.errors[0].message);
+      }
+    }
+  };
+  
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
